ab4d34cd23187e37f7f65892d7835a9497847916
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-09-28 17:40:48 +0300

    Added length

--- TeamCode/src/main/kotlin/com/hamosad/lib/math/Measurements.kt
+++ TeamCode/src/main/kotlin/com/hamosad/lib/math/Measurements.kt
@@ -1,3 +1,20 @@
 package com.hamosad.lib.math
 
-data class Length()
+private const val INCH_TO_METER_RATIO = 0.0254
+
+class Length private constructor(private var lengthMeters: Double) {
+    companion object {
+        fun fromMeters(lengthMeters: Double): Length = Length(lengthMeters)
+
+        fun fromInches(lengthInches: Double): Length = Length(lengthInches * INCH_TO_METER_RATIO)
+
+        fun fromCentimeters(lengthCentimeters: Double): Length = Length(lengthCentimeters / 100)
+
+        fun fromMillimeters(lengthMillimeters: Double): Length = Length(lengthMillimeters / 1000)
+    }
+
+    fun asMeters() = lengthMeters
+    fun asInches() = lengthMeters / INCH_TO_METER_RATIO
+    fun asCentimeters() = lengthMeters * 100
+    fun asMillimeters() = lengthMeters * 1000
+}
