292c32c27ad8b4981eb187f132a52e2747eb3aa5
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-10-21 10:57:50 +0300

    Added iterators to command scheduler instead of for loops

--- TeamCode/src/main/kotlin/com/hamosad/lib/commands/CommandScheduler.kt
+++ TeamCode/src/main/kotlin/com/hamosad/lib/commands/CommandScheduler.kt
@@ -5,7 +5,11 @@ class CommandScheduler(val subsystems: List<Subsystem>) {
     val activeCommands: MutableList<Command> = mutableListOf()
 
     fun scheduleCommand(command: Command) {
-        for (activeCommand in activeCommands) {
+        val iterator = activeCommands.iterator()
+
+        while (iterator.hasNext()) {
+            val activeCommand = iterator.next()
+
             if (activeCommand.requirements.any {it in command.requirements}) {
                 activeCommand.onEnd(true)
                 activeCommands.remove(activeCommand)
@@ -16,13 +20,16 @@ class CommandScheduler(val subsystems: List<Subsystem>) {
     }
 
     fun execute() {
-        for (command in activeCommands) {
-            command.execute()
-            if (command.isFinished()) {
-                command.onEnd(false)
-                activeCommands.remove(command)
+        val iterator = activeCommands.iterator()
+            while (iterator.hasNext()) {
+                val command = iterator.next()
+
+                command.execute()
+                if (command.isFinished()) {
+                    command.onEnd(false)
+                    activeCommands.remove(command)
+                }
             }
-        }
 
         for (subsystem in subsystems) {
             subsystem.periodic()
