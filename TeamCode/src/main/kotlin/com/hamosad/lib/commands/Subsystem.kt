2cb68130c82d7387f9c4e79a820ac4fac5266690
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-11-02 17:14:18 +0200

    Subsystem comment

--- TeamCode/src/main/kotlin/com/hamosad/lib/commands/Subsystem.kt
+++ TeamCode/src/main/kotlin/com/hamosad/lib/commands/Subsystem.kt
@@ -3,8 +3,7 @@ package com.hamosad.lib.commands
 import com.qualcomm.robotcore.hardware.HardwareMap
 
 abstract class Subsystem(var hardwareMap: HardwareMap) {
-
-
+    /** If you want to override it, make sure to call super.restartSubsystem() in the start for the subsystem to function correctly. */
     fun restartSubsystem(newHardwareMap: HardwareMap) {
         hardwareMap = newHardwareMap
     }
