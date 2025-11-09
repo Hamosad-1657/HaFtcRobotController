86f400e523b7ebd9050ed0cf084a648b15143221
Hamosad 1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-11-09 22:10:19 +0200

    AMIT: finished HaCamera and started working on apriltag camera

---
+++ TeamCode/src/main/kotlin/com/hamosad/lib/vision/HaAprilTagCamera.kt
@@ -0,0 +1,21 @@
+package com.hamosad.lib.vision
+
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
+
+//class HaAprilTagCamera(hardwareMap: HardwareMap, name: String, tagFamily: AprilTagProcessor.TagFamily): HaCamera {
+//    private val aprilTagProcessorBuilder: AprilTagProcessor.Builder = AprilTagProcessor.Builder()
+//
+//    init {
+//        // Define Processor Builder
+//        aprilTagProcessorBuilder.setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
+//        aprilTagProcessorBuilder.setDrawTagID(true)
+//        aprilTagProcessorBuilder.setDrawTagOutline(true)
+//        aprilTagProcessorBuilder.setDrawAxes(true)
+//        aprilTagProcessorBuilder.setDrawCubeProjection(true)
+//        aprilTagProcessorBuilder.setCameraPose(cameraPosition, cameraOrientation)
+//        aprilTagProcessorBuilder.setTagFamily(tagFamily)
+//    }
+//
+//}
