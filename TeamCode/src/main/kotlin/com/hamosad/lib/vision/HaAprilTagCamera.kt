f5fc8bec9347a6b17210a3e30753733fe7aee03f
Hamosad 1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-11-15 23:04:13 +0200

    finished haAprilTagCamera

--- TeamCode/src/main/kotlin/com/hamosad/lib/vision/HaAprilTagCamera.kt
+++ TeamCode/src/main/kotlin/com/hamosad/lib/vision/HaAprilTagCamera.kt
@@ -1,36 +1,42 @@
 package com.hamosad.lib.vision
 
-import android.graphics.drawable.GradientDrawable
 import com.hamosad.lib.math.Length
-import com.hamosad.lib.math.Rotation2d
+import com.hamosad.lib.math.Pose2d
 import com.hamosad.lib.math.Rotation3d
+import com.hamosad.lib.math.Rotation2d
+import com.hamosad.lib.math.Translation2d
 import com.hamosad.lib.math.Translation3d
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.robot.Robot
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation
-import org.firstinspires.ftc.robotcore.external.navigation.Pose3D
 import org.firstinspires.ftc.robotcore.external.navigation.Position
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
+import java.util.ArrayList
 import kotlin.system.measureNanoTime
 
-data class stdDevs(
+data class RobotPoseStdDevs(
     val translationX: Double,
     val translationY: Double,
-    val rotation: Double,
+    val rotation: Double
+)
+
+data class AprilTagsStdDevs(
+    val oneTag: RobotPoseStdDevs,
+    val twoTags: RobotPoseStdDevs,
 )
 
 class HaAprilTagCamera(
     hardwareMap: HardwareMap,
     name: String,
-    val maxTrustRange: Length,
+    private val maxTrustRange: Length,
+    private val maxDecisionMargin: Double,
     cameraPositionMeters: Translation3d,
     cameraOrientation: Rotation3d,
-    val stdDevs: stdDevs,
+    private val aprilTagStdDevs: AprilTagsStdDevs,
     tagFamily: AprilTagProcessor.TagFamily = AprilTagProcessor.TagFamily.TAG_36h11
 ):
     HaCamera(hardwareMap, name, aprilTagProcessor) {
@@ -66,8 +72,8 @@ class HaAprilTagCamera(
     }
 
     val hasTargets: Boolean get() = aprilTagProcessor.detections.isNotEmpty()
-    val allTargets get() = aprilTagProcessor.detections
-    val closestTarget get() = allTargets.minByOrNull { it.ftcPose.range }
+    val allTargets: ArrayList<AprilTagDetection?>? get() = aprilTagProcessor.detections
+    val closestTarget: AprilTagDetection? get() = if (hasTargets) {allTargets!!.minByOrNull { it!!.ftcPose.range }} else {null}
     val isInRange: Boolean get() {
         if (hasTargets) {
             return closestTarget!!.ftcPose.range < maxTrustRange.asInches
@@ -75,10 +81,42 @@ class HaAprilTagCamera(
         return false
     }
 
+    val poseEstimationStdDevs
+        get() =
+            if (allTargets?.size == 1) {
+                aprilTagStdDevs.oneTag
+            } else {
+                aprilTagStdDevs.twoTags
+            }
+
+    val estimatedPose: Pose2d? get() {
+        if (!hasTargets || !isInRange || closestTarget == null || allTargets == null) null
+        if (maxDecisionMargin < closestTarget!!.decisionMargin) null
+
+        val bestPose = Pose2d(
+            Translation2d(closestTarget!!.robotPose.position.x, closestTarget!!.robotPose.position.y),
+            Rotation2d.fromRadians(closestTarget!!.robotPose.orientation.getYaw(AngleUnit.RADIANS)),
+            RobotPoseStdDevs(0.0, 0.0, 0.0)
+        )
+
+        for (i in allTargets!!) {
+            bestPose.addPoseEstimate(
+                Pose2d(
+                    Translation2d(i!!.robotPose.position.x, i.robotPose.position.y),
+                    Rotation2d.fromRadians(i.robotPose.orientation.getYaw(AngleUnit.RADIANS)),
+                    //Had to use decision margin, no other info about quality of detections. Im so sorry
+                    RobotPoseStdDevs(i.decisionMargin.toDouble(), i.decisionMargin.toDouble(), i.decisionMargin.toDouble())
+                )
+            )
+        }
+
+        return bestPose
+    }
+
     fun isTagDetected(tagId: Int): Boolean {
-        if (hasTargets == true) {
+        if (hasTargets) {
             for (i in allTargets!!) {
-                if (i.id == tagId) {
+                if (i!!.id == tagId) {
                     return true
                 }
             }
