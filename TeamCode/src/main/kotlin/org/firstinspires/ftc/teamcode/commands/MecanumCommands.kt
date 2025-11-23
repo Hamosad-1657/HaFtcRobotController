e2b00598c35a955d5565e33d33f766adad55d896
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-11-23 18:44:49 +0200

    Added test commands

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/MecanumCommands.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/MecanumCommands.kt
@@ -4,7 +4,7 @@ import com.hamosad.lib.commands.Command
 import com.hamosad.lib.commands.runCommand
 import com.hamosad.lib.math.AngularVelocity
 import com.hamosad.lib.math.Translation2d
-//import com.pedropathing.paths.Path
+import com.pedropathing.paths.Path
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.teamcode.pedropathing.PedroConstants
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.ChassisSpeeds
@@ -23,6 +23,14 @@ fun MecanumSubsystem.angularVelocityDriveCommand(
     ))
 }
 
-//fun MecanumSubsystem.followPath(path: Path, hardwareMap: HardwareMap): Command {
+fun MecanumSubsystem.testMotorsCommand(motor: Int): Command = MecanumSubsystem.runCommand {
+    testMotors(motor)
+}
+
+fun MecanumSubsystem.testPIDCommand(): Command = MecanumSubsystem.runCommand {
+    testPID()
+}
+
+//fun MecanumSubsystem.followPathCommand(path: Path, hardwareMap: HardwareMap): Command {
 //    val follower = PedroConstants.createFollower(hardwareMap)
 //}
